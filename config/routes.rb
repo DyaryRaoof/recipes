7626df2c4342da8fc055df1f8486831f931811aa
mustabbas2000@gmail.com <mustabbas2000@gmail.com>
2022-02-08 00:52:01 +0300

    Implement Recipies list part

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'recipes#index', as: "all_recipes"
+  get "/recipes/:id", to: "recipes#show", as: "recipes_show"
+  delete '/recipes/:id/destroy', to: 'recipes#destroy', as: "destroy_recipes"
 end
