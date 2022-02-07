7eea026133297394ab37ab1fdbc315ec0aa75479
mustabbas2000@gmail.com <mustabbas2000@gmail.com>
2022-02-08 01:01:38 +0300

    Fix linters

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,17 +1,14 @@
 class RecipesController < ApplicationController
-    def index
-        @recipe = Recipe.all
-        puts @recipe
-    end
+  def index
+    @recipe = Recipe.all
+    puts @recipe
+  end
 
-    def show
-        
-    end
+  def show; end
 
-    def destroy
-        Recipe.find(params[:id]).destroy
-        redirect_to all_recipes_path(params[:user_id])
-        flash[:alert] = 'Your recipe was successfully deleted!'
-    end
-
+  def destroy
+    Recipe.find(params[:id]).destroy
+    redirect_to all_recipes_path(params[:user_id])
+    flash[:alert] = 'Your recipe was successfully deleted!'
+  end
 end
