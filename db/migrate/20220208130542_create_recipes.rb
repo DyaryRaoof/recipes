dd04013e17b4cd44a26aa274299f918b7ae96dc1
mustabbas2000@gmail.com <mustabbas2000@gmail.com>
2022-02-08 17:17:03 +0300

    Change table data type

--- db/migrate/20220208130542_create_recipes.rb
+++ db/migrate/20220208130542_create_recipes.rb
@@ -2,10 +2,10 @@ class CreateRecipes < ActiveRecord::Migration[7.0]
   def change
     create_table :recipes do |t|
       t.string :name
-      t.integer :preparation_time
-      t.integer :cooking_time
+      t.float :preparation_time
+      t.float :cooking_time
       t.text :description
-      t.float :public
+      t.boolean :public
       t.integer :user_id
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_07_204230) do
+ActiveRecord::Schema.define(version: 2022_02_08_130542) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "recipes", force: :cascade do |t|
     t.string "name"
-    t.integer "preparation_time"
-    t.integer "cooking_time"
+    t.float "preparation_time"
+    t.float "cooking_time"
     t.text "description"
-    t.float "public"
+    t.boolean "public"
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
