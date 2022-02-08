134f54d094dc1f053ce159120504177918f4c1cf
mustabbas2000@gmail.com <mustabbas2000@gmail.com>
2022-02-09 00:37:50 +0300

    Add Recipe details page

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2022_02_07_202251) do
+ActiveRecord::Schema.define(version: 2022_02_08_205113) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
 
   create_table "foods", force: :cascade do |t|
     t.string "name"
@@ -33,10 +31,11 @@ ActiveRecord::Schema.define(version: 2022_02_07_202251) do
     t.float "cooking_time"
     t.text "description"
     t.boolean "public"
-    t.integer "user_id"
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-
+    t.index ["user_id"], name: "index_recipes_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -61,4 +60,5 @@ ActiveRecord::Schema.define(version: 2022_02_07_202251) do
   end
 
   add_foreign_key "foods", "users"
+  add_foreign_key "recipes", "users"
 end
