de305424d3904a579525b964758f422478e2a8f4
DyaryRaoof <dyary2020@gmail.com>
2022-02-08 23:44:55 +0300

    Add public recipes page

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2022_02_07_202251) do
+ActiveRecord::Schema.define(version: 2022_02_08_190752) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
 
   create_table "foods", force: :cascade do |t|
     t.string "name"
@@ -27,6 +25,16 @@ ActiveRecord::Schema.define(version: 2022_02_07_202251) do
     t.index ["user_id"], name: "index_foods_on_user_id"
   end
 
+  create_table "recipe_foods", force: :cascade do |t|
+    t.integer "quantity"
+    t.bigint "recipe_id", null: false
+    t.bigint "food_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["food_id"], name: "index_recipe_foods_on_food_id"
+    t.index ["recipe_id"], name: "index_recipe_foods_on_recipe_id"
+  end
+
   create_table "recipes", force: :cascade do |t|
     t.string "name"
     t.float "preparation_time"
@@ -36,7 +44,7 @@ ActiveRecord::Schema.define(version: 2022_02_07_202251) do
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -61,4 +69,6 @@ ActiveRecord::Schema.define(version: 2022_02_07_202251) do
   end
 
   add_foreign_key "foods", "users"
+  add_foreign_key "recipe_foods", "foods"
+  add_foreign_key "recipe_foods", "recipes"
 end
