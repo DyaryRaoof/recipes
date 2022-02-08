f47b0d65942da0f826b88a3be974a51579eab08c
DyaryRaoof <dyary2020@gmail.com>
2022-02-08 22:53:09 +0300

    Fix linter errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :confirmable
 
   has_many :recipes
-<<<<<<< HEAD
-=======
   has_many :foods, dependent: :destroy
->>>>>>> 4c22589f0db4043362f7d893b22823048a3b174f
 end
