3ecd18cdfbb12104a46b10f01113472776ed060f
mustabbas2000@gmail.com <mustabbas2000@gmail.com>
2022-02-08 18:57:01 +0300

    Add  form to add recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,6 +6,29 @@ class RecipesController < ApplicationController
 
   def show; end
 
+  def new
+    @recipe = Recipe.new
+    respond_to do |format|
+      format.html { render :new, locals: { recipe: @recipe } }
+    end
+  end
+
+  def create
+    recipe = Recipe.new(params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public,
+                                                       :user_id))
+    respond_to do |format|
+      if recipe.save
+        format.html do
+          flash[:success] = 'recipe saved successfully'
+          redirect_to all_recipes_path
+        end
+      else
+        flash.now[:error] = 'Error: recipe could not be saved'
+        render :new, locals: { recipe: recipe }
+      end
+    end
+  end
+
   def destroy
     Recipe.find(params[:id]).destroy
     redirect_to all_recipes_path(params[:user_id])
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root 'recipes#index', as: "all_recipes"
-  get "/recipes/:id", to: "recipes#show", as: "recipes_show"
+  get "/recipes/", to: "recipes#index", as: "all_recipes"
+  get "/recipes/show/:id", to: "recipes#show", as: "recipes_show"
+  get "/recipes/new", to: "recipes#new", as: "new_recipes"
+  post '/recipes/create', to: 'recipes#create', as: "create_recipes"
   delete '/recipes/:id/destroy', to: 'recipes#destroy', as: "destroy_recipes"
 end
