8532023961cf8bbadb0ba9299db1eb2bfab1c02a
DyaryRaoof <dyary2020@gmail.com>
2022-02-08 19:58:16 +0300

    Add seeds to the file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+dyary = User.create!(name: 'Dyary Raoof', email: 'dyary2020@gmail.com', password: '123456', password_confirmation: '123456')
+mustafa = User.create!(name: 'Dyary Raoof', email: 'mustafa@gmail.com', password: '123456', password_confirmation: '123456')
+
+mustafa.foods.create!(name: 'Apple', measurement_unit: 'kg', price: 1.00);
+mustafa.foods.create!(name: 'Tomato', measurement_unit: 'kg', price: 2.00);
+
+dyary.foods.create!(name: 'Apple', measurement_unit: 'kg', price: 1.00);
+dyary.foods.create!(name: 'Tomato', measurement_unit: 'kg', price: 2.00);
+dyary.foods.create!(name: 'Potato', measurement_unit: 'kg', price: 3.00);
+dyary.foods.create!(name: 'Lemon', measurement_unit: 'kg', price: 1.5);
